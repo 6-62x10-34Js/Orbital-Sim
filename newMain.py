7fdd5ecdb2e65ba1995505695153ca81e2a2628c
Georg <georg.bluethner@outlook.com>
2023-03-23 15:56:38 +0100

    fix animimation plot

--- newMain.py
+++ newMain.py
@@ -6,6 +6,9 @@ from matplotlib import animation
 from matplotlib.animation import FuncAnimation
 
 # Orbital parameters of the satellite
+AU = 149597870700 #meters in 1 AU
+my_0 = 1.327E20 #heliocentric gravitational constant m^3/s^-2
+my = AU / my_0
 a = 0.5959  # semimajor axis in AU
 e = 0.5301  # eccentricity
 inc = 33  # inclination in degrees
@@ -28,11 +31,19 @@ def calc_satellite_pos(t):
         E = E_new
 
     # Calculate the true anomaly and distance
-    theta = 2 * np.arctan2(np.sqrt(1 + e) * np.sin(E / 2), np.sqrt(1 - e) * np.cos(E / 2))
+    # theta = 2 * np.arctan2(np.sqrt(1 + e) * np.sin(E / 2), np.sqrt(1 - e) * np.cos(E / 2))
+    beta = e / 1 + np.sqrt(1-e**2)
+    theta = E + 2 * np.arctan2(beta * np.sin(E), 1 - beta * np.cos(E))
+
+    # Distance between the focus of attraction and the orbiting body
+
     r = a * (1 - e ** 2) / (1 + e * np.cos(theta))
-    sin_v = np.sqrt(1 - e ** 2) * np.sin(E) / (1 - e * np.cos(E))
-    cos_v = (np.cos(E) - e) / (1 - e * np.cos(E))
-    v = np.arctan2(sin_v, cos_v)
+
+
+    # sin_v = np.sqrt(1 - e ** 2) * np.sin(E) / (1 - e * np.cos(E))
+    # cos_v = (np.cos(E) - e) / (1 - e * np.cos(E))
+    # # true anomaly?
+    # v = np.arctan2(sin_v, cos_v)
 
     # Calculate the position vector in the orbital frame
     x_orb = r * np.cos(theta + np.radians(peri_arg))
@@ -44,20 +55,26 @@ def calc_satellite_pos(t):
     y = y_orb * np.cos(np.radians(inc))
     z = y_orb * np.sin(np.radians(inc))
 
+    # Velocity try 3
+    v = np.sqrt(my * (2/r*AU - 1/a*AU)) # in AU / s
+    v_mag = v
+
     # Calculate the velocity of the satellite in Cartesian coordinates
     n = 2 * np.pi / (period * 24 * 60 * 60)
-    vx = -r * n * np.sin(v)
-    vy = r * n * (e + np.cos(v)) * np.cos(inc) / np.sqrt(1 - e ** 2)
-    vz = r * n * (e + np.cos(v)) * np.sin(inc) / np.sqrt(1 - e ** 2)
+    vx = -r * n * np.sin(theta)
+    vy = r * n * (e + np.cos(theta)) * np.cos(inc) / np.sqrt(1 - e ** 2)
+    vz = r * n * (e + np.cos(theta)) * np.sin(inc) / np.sqrt(1 - e ** 2)
     # print('x:'+ str(vx) +'y:' + str(vy) + 'z:'+ str(vz))
-    v_mag = r * np.sqrt(vx ** 2 + vy ** 2 + vz ** 2)
+    # v_mag = r * np.sqrt(vx ** 2 + vy ** 2 + vz ** 2)
 
     return x, y, z, v_mag, r
 
 
 # Create a 3D plot and add the Sun as a fixed point at the origin
 fig = plt.figure(figsize=(12, 6))
-ax = fig.add_subplot(1, 2, 1, projection='3d')
+ax = fig.add_subplot(121, projection='3d')
+ax2 = fig.add_subplot(222)
+ax3 = fig.add_subplot(224)
 ax.set_xlim3d(-1, 1)
 ax.set_ylim3d(-1, 1)
 ax.set_zlim3d(-1, 1)
@@ -71,11 +88,11 @@ def init():
 
 
 # Create the velocity subplot
-ax2 = fig.add_subplot(1, 2, 2)
+
 line, = ax2.plot([], [], 'r')
 xdata, ydata = [], []
 
-ax3 = fig.add_subplot(2, 2, 2)
+
 line2, = ax3.plot([], [], 'b')
 xdata2, ydata2 = [], []
 
@@ -115,7 +132,7 @@ def update(frame):
 
     # Update the velocity subplot
     ax2.set_xlim(t-t, 5 + t)
-    ax2.set_ylim(0, 1E-6)
+    ax2.set_ylim(5, 50)
     print(v_mag)
     ax2.set_xlabel('Time')
     ax2.set_ylabel('Velocity')
